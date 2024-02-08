77e3c27b5fbae23d51a32f3965e063616eeec90f
Cullen <bongsooma@gmail.com>
2024-02-08 16:16:30 +0900

    card

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,11 +1,11 @@
-
+import profilePic from './assets/profile-pic2.png'
 function Card(){
     
     return(
         <div className="card">
-            <img src="" alt="" />'
-            <h2>Cullen</h2>
-            <p>I am a full stack developer</p>
+            <img className='card-image' src={profilePic} alt="profile-picture"></img>
+            <h2 className='card-title'>Cullen</h2>
+            <p className='card-text'>I am a full stack developer</p>
 
         </div>
     )
