74fc7c6bed70135a13f95080205183a310875e9f
Cullen <bongsooma@gmail.com>
2024-02-08 15:43:39 +0900

    pracitce

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,15 @@
+
+function Card(){
+    
+    return(
+        <div className="card">
+            <img src="" alt="" />'
+            <h2>Cullen</h2>
+            <p>I am a full stack developer</p>
+
+        </div>
+    )
+
+}
+
+export default Card
