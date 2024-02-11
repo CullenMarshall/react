2bb09e3cc7f073ac0e3aaff42e244b07561a83fe
Cullen <bongsooma@gmail.com>
2024-02-11 09:30:26 +0900

    renderlists pt.1

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,15 @@
-import UserGreeting from './UserGreeting.jsx';
+import List from './List.jsx';
 
 function App() {
-  return(
 
-    <UserGreeting isLoggedIn={true} username="cunt"/>
+        const fruits =[{id:1, name:"apple",calories: 95},  
+                   {id:2, name: "orange", calories: 45}, 
+                   {id:3, name: "banana", calories: 105}, 
+                   {id:4, name: "coconut", calories: 159}, 
+                   {id:5, name: "pineapple" ,calories: 37}];
 
-  );
-};
+        return(<List items={fruits} category="Fruits"/>)
+
+}
 
 export default App;
